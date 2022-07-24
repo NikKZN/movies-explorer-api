aa25fd66f5539142cfcfd1c94f74e36ec7f4b259
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-07-24 20:34:17 +0300

    feat: add movieSchema

--- models/movie.js
+++ models/movie.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const { validURL } = require('../utils/validURL');
 
-const cardSchema = new mongoose.Schema({
+const movieSchema = new mongoose.Schema({
   country: {
     type: String,
     required: true,
@@ -66,4 +66,4 @@ const cardSchema = new mongoose.Schema({
   },
 });
 
-module.exports = mongoose.model('card', cardSchema);
+module.exports = mongoose.model('movie', movieSchema);
