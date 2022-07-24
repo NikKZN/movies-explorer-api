12f8f2b4f325c6b48ee342d62b16f7c3bbaf3cc3
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-07-24 20:35:20 +0300

    feat: add constants and validURL

---
+++ utils/constants.js
@@ -0,0 +1,12 @@
+// ---Проверка URL
+const regex = /^https?:\/\/(www\.)?[a-zA-Z\d-]+\.[\w\d\-._~:/?#[\]@!$&'()*+,;=]{2,}#?$/;
+
+// ---Массив доменов, с которых разрешены кросс-доменные запросы
+const allowedCors = [
+  'https://',
+  'http://',
+  'localhost:3000',
+  'http://localhost:3000',
+];
+
+module.exports = { regex, allowedCors };
