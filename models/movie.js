55bddb0afa6ea95b59549ca5ef546419d024c3ae
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-07-24 20:33:10 +0300

    feat: add movieSchema

---
+++ models/movie.js
@@ -0,0 +1,69 @@
+const mongoose = require('mongoose');
+const { validURL } = require('../utils/validURL');
+
+const cardSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validURL,
+      message: 'Неверный формат URL!',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validURL,
+      message: 'Неверный формат URL!',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validURL,
+      message: 'Неверный формат URL!',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Schema.Types.ObjectId,
+    // ref: 'user',
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('card', cardSchema);
