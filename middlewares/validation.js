e098205cda417717dc6522c08be772e440d4dc8d
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-07-27 20:56:14 +0300

    feat: add validation

---
+++ middlewares/validation.js
@@ -0,0 +1,54 @@
+const { celebrate, Joi } = require('celebrate');
+const { regex } = require('../utils/constants');
+
+const signupValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().min(2).max(30).required(),
+  }),
+});
+
+const signinValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+});
+
+const updateProfileValidation = celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+});
+
+const deleteMovieByIdValidation = celebrate({
+  params: Joi.object().keys({
+    movieId: Joi.string().required().length(24).hex(),
+  }),
+});
+
+const createMovieValidation = celebrate({
+  body: Joi.object().keys({
+    country: Joi.string().required(),
+    director: Joi.string().required(),
+    duration: Joi.number().required(),
+    year: Joi.string().required(),
+    description: Joi.string().required(),
+    image: Joi.string().required().pattern(regex),
+    trailerLink: Joi.string().required().pattern(regex),
+    thumbnail: Joi.string().required().pattern(regex),
+    movieId: Joi.number().required(),
+    nameRU: Joi.string().required(),
+    nameEN: Joi.string().required(),
+  }),
+});
+
+module.exports = {
+  signupValidation,
+  signinValidation,
+  updateProfileValidation,
+  createMovieValidation,
+  deleteMovieByIdValidation,
+};
