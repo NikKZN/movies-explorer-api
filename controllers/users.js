2b871bc9540d854f409fb5538a0c0778b678e603
Nikolay Petrovskiy <n.kzn@yandex.ru>
2022-07-27 20:54:19 +0300

    feat: add controllers

---
+++ controllers/users.js
@@ -0,0 +1,90 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+const BadRequestError = require('../errors/BadRequestError');
+const NotFoundError = require('../errors/NotFoundError');
+const ConflictError = require('../errors/ConflictError');
+
+const { NODE_ENV, JWT_SECRET } = process.env;
+
+const SALT_ROUNDS = 10;
+
+module.exports.getUser = (req, res, next) => {
+  User.findById(req.user._id)
+    .then((user) => res.send(user))
+    .catch(next);
+};
+
+module.exports.updateProfile = (req, res, next) => {
+  const { name, email } = req.body;
+  User.findByIdAndUpdate(req.user._id, { name, email }, { new: true, runValidators: true })
+    .then((user) => {
+      if (!user) {
+        throw new NotFoundError('Пользователь с указанным _id не найден!');
+      }
+      return res.send(user);
+    })
+    .catch((err) => {
+      if (err.name === 'CastError' || err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при обновлении профиля!'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.createUser = (req, res, next) => {
+  const {
+    name,
+    email,
+    password,
+  } = req.body;
+
+  bcrypt.hash(password, SALT_ROUNDS)
+    .then((hash) => User.create({
+      name,
+      email,
+      password: hash,
+    }))
+    .then((newUser) => {
+      res.status(201).send({
+        name: newUser.name,
+        email: newUser.email,
+        _id: newUser._id,
+      });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        next(new BadRequestError('Переданы некорректные данные при создании пользователя!'));
+      } else if (err.code === 11000) {
+        next(new ConflictError('Данный Email уже зарегистрирован!'));
+      } else {
+        next(err);
+      }
+    });
+};
+
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+      res
+        .cookie('token', token, {
+          httpOnly: true,
+          maxAge: 3600000,
+          sameSite: true,
+        })
+        .send({ token });
+    })
+    .catch(next);
+};
+
+module.exports.logout = (req, res) => {
+  res.clearCookie('token').send({ message: 'Сессия завершена.' });
+};
